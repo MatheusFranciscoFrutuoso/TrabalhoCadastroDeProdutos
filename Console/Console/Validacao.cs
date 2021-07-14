da1af4d6f2cf4250dbb3ab2517135f47ca900f8b
Matheus Frutuoso <matheusfx5@gmail.com>
2021-07-14 16:22:30 -0300

    Implementação de cadastros de Produto e Categoria e Listagem

---
+++ Console/Console/Validacao.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console
+{
+    class Validacao
+    {
+        public static string ValidaStringEmBranco()
+        {
+            string dado = System.Console.ReadLine().Trim();
+            while (String.IsNullOrEmpty(dado))
+            {
+                System.Console.WriteLine("Valor invalido");
+                dado = System.Console.ReadLine().Trim();
+            }
+            return dado;
+        }
+        public static int ValidaInteiros()
+        {
+            do
+            {
+                try
+                {
+                    int dado = Convert.ToInt32(System.Console.ReadLine());
+                    return dado;
+                }
+                catch (FormatException)
+                {
+                    System.Console.WriteLine("Dado invalido");
+                }
+            } while (true);
+        }
+
+    }
+}
